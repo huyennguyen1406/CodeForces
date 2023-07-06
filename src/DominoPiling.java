a3cbb66b3957d9bdc1942f1128bb41c9389392ee
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-07-07 02:02:57 +0700

    refactor

--- src/DominoPiling.java
+++ src/DominoPiling.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
 
-public class Statement {
+public class DominoPiling {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int M = sc.nextInt();
         int N = sc.nextInt();
         int max_dominoes;
         if (M % 2 == 1 && N % 2 == 1) {
-            // If both dimensions are odd, we can place one vertical domino in the middle
             max_dominoes = (M*N - 1) / 2;
         } else {
-            // Otherwise, we can cover the entire board with horizontal dominoes
             max_dominoes = (M*N) / 2;
         }
         System.out.println(max_dominoes);
+        sc.close();
     }
 }
